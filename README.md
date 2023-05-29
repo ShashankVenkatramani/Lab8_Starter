c94e80739aed970f9073b0899e2684347129d69f
ShashankVenkatramani <cjvshashank@gmail.com>
2023-05-28 21:52:35 -0700

    Finished lab

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab 8 - Starter
+
+Responding to question 1, you should fit automated testing within a github action that runs whenever code is pushed, since that allows for incremental development to recieve feedback on every advance, to ensure that no prior functionality breaks nor current functionality failing to cover all cases tested.
+
+Responding to question 2, you would not use end to end tests to check if a function is returning the correct output, for a smaller unit like a function you would use a unit test.
+
+Responding to question 3, you would not use a unit test since this is more of an end to end test since it is accross the system from one user to another
+
+Responding to question 4, you would use a unit test since blocking messages over a certain character length is a unit piece code and not accross the system.
+
+![Screenshots of Running Test](runtest.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,90 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+test('Phone Pass Case 1', () => {
+    expect(functions.isPhoneNumber("408-500-8000")).toBe(true);
+});
+
+test('Phone Pass Case 2', () => {
+    expect(functions.isPhoneNumber("424-244-4586")).toBe(true);
+});
+
+test('Phone Fail Case 1', () => {
+    expect(functions.isPhoneNumber("4242444586")).toBe(false);
+});
+
+test('Phone Fail Case 2', () => {
+    expect(functions.isPhoneNumber("")).toBe(false);
+});
+
+
+
+test('Email Pass Case 1', () => {
+    expect(functions.isEmail("test@test.com")).toBe(true);
+});
+
+test('Email Pass Case 2', () => {
+    expect(functions.isEmail("abcd@abcd.net")).toBe(true);
+});
+
+test('Email Fail Case 1', () => {
+    expect(functions.isEmail("abc.com")).toBe(false);
+});
+
+test('Email Fail Case 2', () => {
+    expect(functions.isEmail("")).toBe(false);
+});
+
+
+
+test('Strong Password Pass Case 1', () => {
+    expect(functions.isStrongPassword("abcd")).toBe(true);
+});
+
+test('Strong Password Pass Case 2', () => {
+    expect(functions.isStrongPassword("abcdEFGH")).toBe(true);
+});
+
+test('Strong Password Fail Case 1', () => {
+    expect(functions.isStrongPassword("ab")).toBe(false);
+});
+
+test('Strong Password Fail Case 2', () => {
+    expect(functions.isStrongPassword("fasdfasdfasdfasdfasdfasdfasdf")).toBe(false);
+});
+
+
+
+test('Is Date Pass Case 1', () => {
+    expect(functions.isDate("12/30/4000")).toBe(true);
+});
+
+test('Is Date Pass Case 2', () => {
+    expect(functions.isDate("1/30/4000")).toBe(true);
+});
+
+test('Is Date Fail Case 1', () => {
+    expect(functions.isDate("12/30/400")).toBe(false);
+});
+
+test('Is Date Fail Case 2', () => {
+    expect(functions.isDate("")).toBe(false);
+});
+
+
+
+test('Is Hex Color Pass Case 1', () => {
+    expect(functions.isHexColor("#FFF")).toBe(true);
+});
+
+test('Is Hex Color Pass Case 2', () => {
+    expect(functions.isHexColor("#FFFFFF")).toBe(true);
+});
+
+test('Is Hex Color Fail Case 1', () => {
+    expect(functions.isHexColor("#FFFFF")).toBe(false);
+});
+
+test('Is Hex Color Fail Case 2', () => {
+    expect(functions.isHexColor("")).toBe(false);
+});
